e7dd1fca89ab4e69dde4d0ea72d80db846ef5fd0
Joshua Rodriguez <joshrodriguez17@gmail.com>
2017-02-14 17:31:05 -0600

    JoshuaIsaiah.com v2.0 - Combined About and Home, removed DJ section, added Contact Page, completed entire responsive webpage.

--- script.js
+++ script.js
@@ -1,3 +1,55 @@
+$(function() {
+
+	// Get the form.
+	var form = $('#contactform');
+
+	// Get the messages div.
+	var formMessages = $('#form-messages');
+
+	// Set up an event listener for the contact form.
+	$(form).submit(function(e) {
+		// Stop the browser from submitting the form.
+		e.preventDefault();
+
+		// Serialize the form data.
+		var formData = $(form).serialize();
+
+		// Submit the form using AJAX.
+		$.ajax({
+			type: 'POST',
+			url: $(form).attr('action'),
+			data: formData
+		})
+		.done(function(response) {
+			// Make sure that the formMessages div has the 'success' class.
+			$(formMessages).removeClass('error');
+			$(formMessages).addClass('success');
+
+			// Set the message text.
+			$(formMessages).text(response);
+
+			// Clear the form.
+			$('#name').val('');
+			$('#email').val('');
+			$('#message').val('');
+		})
+		.fail(function(data) {
+			// Make sure that the formMessages div has the 'error' class.
+			$(formMessages).removeClass('success');
+			$(formMessages).addClass('error');
+
+			// Set the message text.
+			if (data.responseText !== '') {
+				$(formMessages).text(data.responseText);
+			} else {
+				$(formMessages).text('Oops! An error occured and your message could not be sent.');
+			}
+		});
+
+	});
+
+});
+
 $(document).ready(function() {
 	$(".fancybox").fancybox( {
 		openEffect	: 'none',
@@ -11,11 +63,12 @@ $(document).ready(function() {
 	});
 	
 	$(window).resize(function() {
-		if($(window).width() > 590) {
+		if($(window).width() > 520) {
 			$('#responsive').hide();
 		}
 	});
-	
-	$("#djpic").css("display", "inline-block");
+
+	$('#darrow').smoothScroll();
+
 });
 
